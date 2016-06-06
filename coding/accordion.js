1059f238a8d3041403f74e0228490dfdda9a0d43
cpfhome <makingdifference@163.com>
2016-06-07 00:06:44 +0800

    accordion demo done

--- coding/accordion.js
+++ coding/accordion.js
@@ -1,27 +1,25 @@
-angular.module('ui.bootstrap.demo', [])
-	.controller('accordionDemoCtrl', function($scope){
-		$scope.oneAtTime = true;
+angular.module('ui.bootstrap.demo')
+.controller('AccordionDemoCtrl', function($scope) {
+  $scope.oneAtATime = true;
 
-		$scope.groups = [
-			{
-				title:'Dynamic Group Header - 1',
-				content: 'Dynamic Group Body -1'
-			},
-			{
-				title:'Dynamic Group Header - 2',
-				content: 'Dynamic Group Body -2'
-			}
-		];
-		$scope.items = ['Item 1', 'Item 2', 'Item 3'];
+  $scope.groups = [{
+    title: 'Dynamic Group Header - 1',
+    content: 'Dynamic Group Body - 1'
+  }, {
+    title: 'Dynamic Group Header - 2',
+    content: 'Dynamic Group Body - 2'
+  }];
 
-		$scope.addItem = function(){
-			var newItemNo = $scope.items.length + 1;
-			$scope.items.push('Item ' + newItemNo);
-		};
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
 
-		$scope.status = {
-			isCustomHeaderOpen: false;
-			isFirstOpen: true,
-			isFirstDisabled: false
-		};
-	});
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
+
+  $scope.status = {
+    isCustomHeaderOpen: false,
+    isFirstOpen: true,
+    isFirstDisabled: false
+  };
+});
